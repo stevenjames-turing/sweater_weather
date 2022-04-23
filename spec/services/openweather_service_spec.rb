34ea02cf10e1d19403a798f03271948428e607ff
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-23 13:24:15 -0500

    Test: api returns hash of data

--- spec/services/openweather_service_spec.rb
+++ spec/services/openweather_service_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe OpenweatherService do
   context 'class methods' do 
-    context '#' do
-
+    context '#all_weather(latitude, longitude)' do
+      before(:each) do 
+        @weather_data = OpenweatherService.all_weather(38.6270, -90.1994)
+      end
+      it 'returns a hash of weather data' do 
+        expect(@weather_data).to be_a Hash
+      end
     end
   end
 end
