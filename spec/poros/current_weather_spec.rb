a8b20bee872ef62f427aae1b1a9189986ab00b69
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-23 16:23:08 -0500

    Test: it has expected attributes

--- spec/poros/current_weather_spec.rb
+++ spec/poros/current_weather_spec.rb
@@ -35,5 +35,18 @@ RSpec.describe CurrentWeather do
     expect(current).to be_a CurrentWeather
   end
   
+  it 'has all required attributes' do 
+    current = CurrentWeather.new(@weather_data)
   
+    expect(current.datetime).to eq(Time.at(1650747631))
+    expect(current.sunrise).to eq(Time.at(1650712400))
+    expect(current.sunset).to eq(Time.at(1650761070))
+    expect(current.temperature).to eq(84.09)
+    expect(current.feels_like).to eq(83.08)
+    expect(current.humidity).to eq(38)
+    expect(current.uvi).to eq(0)
+    expect(current.visibility).to eq(10000)
+    expect(current.conditions).to eq('broken clouds')
+    expect(current.icon).to eq('04d')
+  end
 end
