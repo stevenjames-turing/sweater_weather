7c41ca4c605074ef381fb76a740c54db685fe721
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-23 16:41:13 -0500

    Feat: current_weather creates a CurrentWeather object with expected attributes

--- app/facades/openweather_facade.rb
+++ app/facades/openweather_facade.rb
@@ -4,7 +4,7 @@ class OpenweatherFacade
     json = OpenweatherService.all_weather(latitude, longitude)
 
     # Creates objects for only the next 5 days
-    @daily_weather = json[:daily][0..4].map do |day_data|
+    daily_weather = json[:daily][0..4].map do |day_data|
       DailyWeather.new(day_data)
     end
   end
@@ -13,8 +13,14 @@ class OpenweatherFacade
     json = OpenweatherService.all_weather(latitude, longitude)
 
     # Creates objects for only the next 8 hours
-    @hourly_weather = json[:hourly][0..7].map do |hour_data|
+    hourly_weather = json[:hourly][0..7].map do |hour_data|
       HourlyWeather.new(hour_data)
     end
+  end
+
+  def self.current_weather(latitude, longitude)
+    json = OpenweatherService.all_weather(latitude, longitude)
+
+    CurrentWeather.new(json[:current])
   end
 end
