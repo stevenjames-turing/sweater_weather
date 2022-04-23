c9f44a4fc402b9242eed40f8ca9948b32cdb1307
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-23 15:33:36 -0500

    Test: daily_weather creates array of DailyWeather objects

--- spec/facades/openweather_facade_spec.rb
+++ spec/facades/openweather_facade_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe OpenweatherFacade do
   context 'class methods' do 
-    context '#' do
+    context '#daily_weather(latitude, longitude)' do
+      before(:each) do 
+        @daily = OpenweatherFacade.daily_weather(38.6270, -90.1994)
+      end
 
+      it 'creates array of DailyWeather objects' do 
+        expect(@daily).to be_an Array
+      end
+
+      
     end
   end
 end
