0f87b627358b4b19467d39fe9d5dd4787d817b6a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-23 13:27:40 -0500

    Test: api returns expected data

--- spec/services/openweather_service_spec.rb
+++ spec/services/openweather_service_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe OpenweatherService do
       before(:each) do 
         @weather_data = OpenweatherService.all_weather(38.6270, -90.1994)
       end
+
       it 'returns a hash of weather data' do 
         expect(@weather_data).to be_a Hash
+      end
+
+      it 'hash has expected data' do 
+        expect(@weather_data.keys).to eq([:lat, :lon, :timezone, :timezone_offset, :current, :hourly, :daily])
       end
     end
   end
