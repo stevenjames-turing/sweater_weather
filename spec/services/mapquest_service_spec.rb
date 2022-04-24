d44e04d9522726f171ae935ff1b556855e4cf225
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-24 11:17:56 -0500

    Test: results include latitutde and longitude data for location

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe MapquestService, :vcr do
     context '#get_city_lat_lon(city)' do
       before(:each) do 
         @response = MapquestService.get_city_lat_lon('St. Louis, MO')
-        @location_data = @response[:results]
+        @location_data = @response[:results][0]
       end
 
       it 'returns a hash of weather data' do 
@@ -14,6 +14,10 @@ RSpec.describe MapquestService, :vcr do
 
       it 'hash has expected data' do 
         expect(@response.keys).to eq([:info, :options, :results])
+      end
+
+      it 'results include latitude and longitude' do 
+        expect(@location_data[:locations][0][:latLng].keys).to eq([:lat, :lng])
       end
     end
   end
