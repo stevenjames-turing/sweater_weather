efb43834e126b0b4903e5897e257ebb1b5df87de
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-24 11:36:25 -0500

    Refactor: Update tests for limited return from service

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -4,28 +4,23 @@ RSpec.describe MapquestService, :vcr do
   context 'class methods' do 
     context '#get_city_coordinates(city)' do
       before(:each) do 
-        @response = MapquestService.get_city_coordinates('St. Louis, MO')
-        @location_data = @response[:results][0]
+        @location_data = MapquestService.get_city_coordinates('St. Louis, MO')
       end
 
       it 'returns a hash of weather data' do 
-        expect(@response).to be_a Hash
+        expect(@location_data).to be_a Hash
       end
 
       it 'hash has expected data' do 
-        expect(@response.keys).to eq([:info, :options, :results])
-      end
-
-      it 'results include latitude and longitude' do 
-        expect(@location_data[:locations][0][:latLng].keys).to eq([:lat, :lng])
+        expect(@location_data.keys).to eq([:lat, :lng])
       end
 
       it 'latitude and longitude are returned as a Float' do 
-        expect(@location_data[:locations][0][:latLng][:lat]).to be_a Float
-        expect(@location_data[:locations][0][:latLng][:lat]).to eq(38.630276)
+        expect(@location_data[:lat]).to be_a Float
+        expect(@location_data[:lat]).to eq(38.630276)
         
-        expect(@location_data[:locations][0][:latLng][:lng]).to be_a Float
-        expect(@location_data[:locations][0][:latLng][:lng]).to eq(-90.200309)
+        expect(@location_data[:lng]).to be_a Float
+        expect(@location_data[:lng]).to eq(-90.200309)
       end
     end
   end
