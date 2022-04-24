0f0f7cd682de7a200444c4c1f8d02eb9d971ad60
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-24 18:22:41 -0500

    Users#create added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ namespace :api do
     namespace :v1 do 
       resources :backgrounds, only: [:index]
       resources :forecast, only: [:index]
+      resources :users, only: [:create]
     end
   end
 
