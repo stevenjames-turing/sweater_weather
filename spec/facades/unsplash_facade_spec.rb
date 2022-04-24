eb804f75916a6260c3a42e46d615abf0556e5df9
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-24 15:07:43 -0500

    Refactor: update for facade to use location and weather conditions

--- spec/facades/unsplash_facade_spec.rb
+++ spec/facades/unsplash_facade_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe UnsplashFacade, :vcr do
   context 'class methods' do 
     context '#photo_search(query)' do
       before(:each) do 
-        @photo = UnsplashFacade.photo_search('denver colorado cloudy')
+        location = 'denver, co'
+        current_weather_condition = 'scattered clouds'
+        @photo = UnsplashFacade.photo_search(location, current_weather_condition)
       end
 
       it 'creates a Photo object' do 
