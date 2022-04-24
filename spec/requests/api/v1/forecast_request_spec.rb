1e3ea40e77b5887c7a972e894e8d111c38583a37
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-24 13:04:31 -0500

    Test: api returns json of data

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 describe 'Forecast API', :vcr do
-
+  
   context 'forecast#index' do
+    
+    context 'happy path' do 
+      it 'has a successful response' do 
+        get '/api/v1/forecast?location=denver, co'
+        
+        expect(response).to be_successful
+        expect(response).to have_http_status(200)
+      end
+      
+      it 'returns a json of expected data' do 
+        get '/api/v1/forecast?location=denver, co'
+        data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+        expect(data.keys).to eq([:id, :type, :attributes])
+      end
+    end
 
     context 'location param is empty' do 
 
@@ -23,7 +39,7 @@ describe 'Forecast API', :vcr do
     end
     
     context 'location param is missing' do 
-      
+
       it 'returns a 400 error code' do 
         get '/api/v1/forecast?'
         
