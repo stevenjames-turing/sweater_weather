86aca327d568f4a447dcfbcbe415fa2eeb0d8b1a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-24 15:31:25 -0500

    Test: attribute data has correct data type

--- spec/requests/api/v1/backgrounds_request_spec.rb
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -23,8 +23,18 @@ describe 'Backgrounds API', :vcr do
       it 'attributes contain source, image_url, photographer name, and profile' do 
         get '/api/v1/backgrounds?location=denver, co'
         attributes = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
-
+        
         expect(attributes.keys).to eq([:source, :image_url, :photographer, :photographer_profile])
+      end
+      
+      it 'attributes have the correct format type' do 
+        get '/api/v1/backgrounds?location=denver, co'
+        attributes = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+        expect(attributes[:source]).to be_a String
+        expect(attributes[:image_url]).to be_a String
+        expect(attributes[:photographer]).to be_a String
+        expect(attributes[:photographer_profile]).to be_a String
       end
     end 
     
