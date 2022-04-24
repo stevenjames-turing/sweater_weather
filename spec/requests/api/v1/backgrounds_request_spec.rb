430e13261f8939e067f594fe38d8573bda2de23b
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-24 14:53:20 -0500

    Test: returns error if location param is missing

--- spec/requests/api/v1/backgrounds_request_spec.rb
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -22,6 +22,22 @@ describe 'Backgrounds API', :vcr do
       end
     end
     
-    
+    context 'location param is missing' do 
+
+      it 'returns a 400 error code' do 
+        get '/api/v1/backgrounds?'
+        
+        expect(response).to have_http_status(400)
+      end
+      
+      it 'returns error message for invalid parameters' do 
+        get '/api/v1/backgrounds?'
+        expect(response).to have_http_status(400)
+        
+        json = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(json[:error]).to eq({:message=>"invalid parameters"})  
+      end
+    end
   end
 end
