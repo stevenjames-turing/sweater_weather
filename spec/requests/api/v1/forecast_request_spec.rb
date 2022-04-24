ac28fc82f789dcdf229332da464893e625c04113
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-24 12:09:37 -0500

    Test: it will return a 400 error message if location param is missing

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 describe 'Forecast API', :vcr do
+
   context 'forecast#index' do
+
     context 'location param is empty' do 
+
       it 'returns a 400 error code' do 
         get '/api/v1/forecast?location='
         
@@ -11,6 +14,24 @@ describe 'Forecast API', :vcr do
       
       it 'returns error message for invalid parameters' do 
         get '/api/v1/forecast?location='
+        expect(response).to have_http_status(400)
+        
+        json = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(json[:error]).to eq({:message=>"invalid parameters"})  
+      end
+    end
+    
+    context 'location param is missing' do 
+      
+      it 'returns a 400 error code' do 
+        get '/api/v1/forecast?'
+        
+        expect(response).to have_http_status(400)
+      end
+      
+      it 'returns error message for invalid parameters' do 
+        get '/api/v1/forecast?'
         expect(response).to have_http_status(400)
         
         json = JSON.parse(response.body, symbolize_names: true)
