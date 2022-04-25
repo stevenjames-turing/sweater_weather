11ac50d8b136c549d7a69aa6ac7c0d0d45ceb2f5
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-25 15:49:38 -0500

    Test: serializer returns expected data

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -4,40 +4,40 @@ describe 'RoadTrip Endpoint', :vcr do
   
   context 'road_trip#create' do
 
-    # context 'happy path' do 
-    #   before(:each) do 
-    #     user = User.create!(email: 'test_email@test.com', password: 'password123', password_confirmation: 'password123')
-    #     @request_body = {
-    #                       "road_trip": {
-    #                                     "origin": "Denver,CO",
-    #                                     "destination": "Pueblo,CO",
-    #                                     "api_key": "#{user.api_key}"
-    #                                     }
-    #                     }
-    #   end
+    context 'happy path' do 
+      before(:each) do 
+        user = User.create!(email: 'test_email@test.com', password: 'password123', password_confirmation: 'password123')
+        @request_body = {
+                          "road_trip": {
+                                        "origin": "Denver,CO",
+                                        "destination": "Pueblo,CO",
+                                        "api_key": "#{user.api_key}"
+                                        }
+                        }
+      end
       
-    #   it 'has a successful created response' do 
-    #     post '/api/v1/road_trip', :params => @request_body
+      it 'has a successful created response' do 
+        post '/api/v1/road_trip', :params => @request_body
         
-    #     expect(response).to be_successful
-    #     expect(response).to have_http_status(200)
-    #   end
+        expect(response).to be_successful
+        expect(response).to have_http_status(200)
+      end
       
-    #   it 'returns a json of expected data' do 
-    #     post '/api/v1/road_trip', :params => @request_body
+      it 'returns a json of expected data' do 
+        post '/api/v1/road_trip', :params => @request_body
         
-    #     data = JSON.parse(response.body, symbolize_names: true)[:data]
+        data = JSON.parse(response.body, symbolize_names: true)[:data]
         
-    #     expect(data.keys).to eq([:id, :type, :attributes])
-    #   end
+        expect(data.keys).to eq([:id, :type, :attributes])
+      end
       
-    #   it 'attributes contains email and api_key' do 
-    #     post '/api/v1/road_trip', :params => @request_body
+      it 'attributes contains start_city, end_city, travel_time, and weather_at_eta' do 
+        post '/api/v1/road_trip', :params => @request_body
 
-    #     attributes = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+        attributes = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
 
-    #     expect(attributes.keys).to eq([:email, :api_key])
-    #   end
+        expect(attributes.keys).to eq([:start_city, :end_city, :travel_time, :weather_at_eta])
+      end
 
     #   it 'attributes does NOT contain password' do 
     #     post '/api/v1/road_trip', :params => @request_body
@@ -48,7 +48,7 @@ describe 'RoadTrip Endpoint', :vcr do
     #     expect(attributes.has_key?(:password_digest)).to be false 
     #     expect(attributes.has_key?(:password_confirmation)).to be false 
     #   end
-    # end
+    end
 
     context 'params are passed through URL' do 
       before(:each) do 
