03f3da5b16b74ad11a35712daef215b153d34630
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-25 14:54:47 -0500

    Feat: it returns json of route data

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -1,17 +1,26 @@
 class MapquestService
 
   def self.conn 
-    Faraday.new(url: 'http://www.mapquestapi.com/geocoding/v1/') do |faraday|
+    Faraday.new(url: 'http://www.mapquestapi.com/') do |faraday|
       faraday.params['key'] = ENV['mapquest_key']
     end
   end
 
   def self.get_city_coordinates(city)
-    response = conn.get('address') do |request|
+    response = conn.get('geocoding/v1/address') do |request|
       request.params['location'] = city
       request.params['outFormat'] = 'json'
     end
     json = JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:latLng]
+  end
+  
+  def self.directions(start, destination)
+    response = conn.get('directions/v2/route') do |request|
+      request.params['from'] = start
+      request.params['to'] = destination
+      request.params['outFormat'] = 'json'
+    end
+    json = JSON.parse(response.body, symbolize_names: true)
   end
 
 end
