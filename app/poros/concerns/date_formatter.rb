1e4f58e45a11c40594f362f8d64341b5e8985cff
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-25 15:14:01 -0500

    Refactor: change DateFormatter to DateTimeFormatter

--- app/poros/concerns/date_formatter.rb
+++ app/poros/concerns/date_formatter.rb
@@ -1,4 +1,4 @@
-module DateFormatter
+module DateTimeFormatter
   def date_formatter(date_time)
     Time.at(date_time)
   end
--- app/poros/roadtrip.rb
+++ app/poros/roadtrip.rb
@@ -1,4 +1,5 @@
 class Roadtrip 
+  include DateTimeFormatter
 
     attr_reader :id, :start_city, :end_city, :travel_time, :weather_at_eta
 
