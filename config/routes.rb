f1ba8734883b57e23562fba1bf470976ce0dd7aa
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-25 14:27:00 -0500

    Update route for new endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ namespace :api do
       resources :forecast, only: [:index]
       resources :users, only: [:create]
       resources :sessions, only: [:create]
+      resources :road_trip, only: [:create]
     end
   end
 
