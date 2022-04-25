7c1e2492aa76c0d823c641e4729a4cc7f5565f50
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-25 12:17:57 -0500

    Test: it returns error message if food param is empty

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -105,7 +105,21 @@ describe 'Munchies API', :vcr do
         end
       end
 
-     
+      context 'food param' do 
+        it 'returns a 400 error code' do 
+          get '/api/v1/munchies?start=denver,co&destination=pueblo,co&food='
+          
+          expect(response).to have_http_status(400)
+        end
+        it 'returns error message for invalid parameters' do 
+          get '/api/v1/munchies?start=denver,co&destination=pueblo,co&food='
+          expect(response).to have_http_status(400)
+          
+          json = JSON.parse(response.body, symbolize_names: true)
+          
+          expect(json[:error]).to eq({:message=>"invalid parameters"})  
+        end
+      end
     end
   end 
 end
