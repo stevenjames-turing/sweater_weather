d52321d35e1a27d8d108cc9bb6670141ccc58073
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-25 16:32:44 -0500

    Test: it returns impossible route if mapquest returns route error

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -4,6 +4,7 @@ describe 'RoadTrip Endpoint', :vcr do
   
   context 'road_trip#create' do
 
+
     context 'happy path' do 
       before(:each) do 
         user = User.create!(email: 'test_email@test.com', password: 'password123', password_confirmation: 'password123')
@@ -15,14 +16,12 @@ describe 'RoadTrip Endpoint', :vcr do
                                         }
                         }
       end
-      
       it 'has a successful created response' do 
         post '/api/v1/road_trip', :params => @request_body
         
         expect(response).to be_successful
         expect(response).to have_http_status(200)
       end
-      
       it 'returns a json of expected data' do 
         post '/api/v1/road_trip', :params => @request_body
         
@@ -30,7 +29,6 @@ describe 'RoadTrip Endpoint', :vcr do
         
         expect(data.keys).to eq([:id, :type, :attributes])
       end
-      
       it 'attributes contains start_city, end_city, travel_time, and weather_at_eta' do 
         post '/api/v1/road_trip', :params => @request_body
 
@@ -38,7 +36,6 @@ describe 'RoadTrip Endpoint', :vcr do
 
         expect(attributes.keys).to eq([:start_city, :end_city, :travel_time, :weather_at_eta])
       end
-
       it 'attributes does NOT contain password' do 
         post '/api/v1/road_trip', :params => @request_body
 
@@ -50,17 +47,17 @@ describe 'RoadTrip Endpoint', :vcr do
       end
     end
 
+
+
     context 'params are passed through URL' do 
       before(:each) do 
         @user = User.create!(email: 'test_email@test.com', password: 'password123', password_confirmation: 'password123')
       end
-
       it 'returns a 401 error code' do 
         post "/api/v1/road_trip?origin=Denver,CO&destination=Pueblo,CO&api_key=#{@user.api_key}"
 
         expect(response).to have_http_status(400)
       end
-
       it 'returns an error message' do 
         post "/api/v1/road_trip?origin=Denver,CO&destination=Pueblo,CO&api_key=#{@user.api_key}"
 
@@ -69,6 +66,8 @@ describe 'RoadTrip Endpoint', :vcr do
         expect(json[:error]).to eq({:message=>"missing request body"})  
       end
     end
+
+
 
     context 'params are MISSING' do 
       context 'origin param' do 
@@ -81,13 +80,11 @@ describe 'RoadTrip Endpoint', :vcr do
                                           }
                           }
         end
-
         it 'returns a 400 error code' do 
           post '/api/v1/road_trip', :params => @origin_missing
 
           expect(response).to have_http_status(400)
         end
-
         it 'returns an error message' do 
           post '/api/v1/road_trip', :params => @origin_missing
   
@@ -107,13 +104,11 @@ describe 'RoadTrip Endpoint', :vcr do
                                         }
                         }
         end
-
         it 'returns a 400 error code' do 
           post '/api/v1/road_trip', :params => @destination_missing
 
           expect(response).to have_http_status(400)
         end
-
         it 'returns an error message' do 
           post '/api/v1/road_trip', :params => @destination_missing
   
@@ -133,13 +128,11 @@ describe 'RoadTrip Endpoint', :vcr do
                                         }
                         }
         end
-
         it 'returns a 400 error code' do 
           post '/api/v1/road_trip', :params => @api_key_missing
 
           expect(response).to have_http_status(400)
         end
-
         it 'returns an error message' do 
           post '/api/v1/road_trip', :params => @api_key_missing
   
@@ -148,8 +141,9 @@ describe 'RoadTrip Endpoint', :vcr do
           expect(json[:error]).to eq({:message=>"required parameters are missing or empty"})  
         end
       end
-
     end
+
+
 
     context 'params are EMPTY' do 
       context 'origin param' do 
@@ -163,13 +157,11 @@ describe 'RoadTrip Endpoint', :vcr do
                                           }
                           }
         end
-
         it 'returns a 400 error code' do 
           post '/api/v1/road_trip', :params => @empty_origin
 
           expect(response).to have_http_status(400)
         end
-
         it 'returns an error message' do 
           post '/api/v1/road_trip', :params => @empty_origin
   
@@ -190,13 +182,11 @@ describe 'RoadTrip Endpoint', :vcr do
                                           }
                           }
         end
-
         it 'returns a 400 error code' do 
           post '/api/v1/road_trip', :params => @empty_destination
 
           expect(response).to have_http_status(400)
         end
-
         it 'returns an error message' do 
           post '/api/v1/road_trip', :params => @empty_destination
   
@@ -217,13 +207,11 @@ describe 'RoadTrip Endpoint', :vcr do
                                           }
                           }
         end
-
         it 'returns a 400 error code' do 
           post '/api/v1/road_trip', :params => @empty_api_key
 
           expect(response).to have_http_status(400)
         end
-
         it 'returns an error message' do 
           post '/api/v1/road_trip', :params => @empty_api_key
   
@@ -232,7 +220,32 @@ describe 'RoadTrip Endpoint', :vcr do
           expect(json[:error]).to eq({:message=>"required parameters are missing or empty"})  
         end
       end
+    end
 
+
+    context 'route is not possible' do 
+      before(:each) do 
+        user = User.create!(email: 'test_email@test.com', password: 'password123', password_confirmation: 'password123')
+        @request_body = {
+                          "road_trip": {
+                                        "origin": "Denver,CO",
+                                        "destination": "London, UK",
+                                        "api_key": "#{user.api_key}"
+                                        }
+                        }
+      end
+      it 'has a successful response' do 
+        post '/api/v1/road_trip', :params => @request_body
+        
+        expect(response).to be_successful
+        expect(response).to have_http_status(200)
+      end
+      it 'returns impossible route for travel time' do 
+        post '/api/v1/road_trip', :params => @request_body
+        attributes = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+        expect(attributes[:travel_time]).to eq('impossible route')
+      end
     end
   end 
 end 
