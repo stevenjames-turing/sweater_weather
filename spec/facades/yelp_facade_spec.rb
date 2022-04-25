d881bc1afa41fbe5c99fccd6121ecdea800f7274
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-25 11:19:12 -0500

    Test: facade creates restaurant objects

---
+++ spec/facades/yelp_facade_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe YelpFacade, :vcr do
+  context 'class methods' do 
+    context '#business_search(latitude, longitude, term)' do
+      before(:each) do 
+        @restaurant = YelpFacade.business_search(38.630276, -90.200309, 'tacos')
+      end
+
+      it 'creates a Restaurant object' do 
+        expect(@restaurant).to be_a Restaurant
+      end
+
+      it 'each object has expected attributes' do 
+        expect(@restaurant.id).to eq('null')
+        expect(@restaurant.name).to be_a String
+        expect(@restaurant.address).to be_a String
+      end
+    end
+  end
+end
