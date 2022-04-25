fe9526abd8c20396edf39725bd6d6b4d1f54b964
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-25 13:40:55 -0500

    Test: it returns json with expected data

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -14,12 +14,20 @@ describe 'Sessions Endpoint', :vcr do
                                       }
                         }
       end
-
+      
       it 'has a successful created response' do 
         post '/api/v1/sessions', :params => @request_body
         
         expect(response).to be_successful
         expect(response).to have_http_status(200)
+      end
+      
+      it 'returns a json of expected data' do 
+        post '/api/v1/sessions', :params => @request_body
+        
+        data = JSON.parse(response.body, symbolize_names: true)[:data]
+        
+        expect(data.keys).to eq([:id, :type, :attributes])
       end
       
       
