8d988a63aef26968ee5f0a1d95fe4fdd53608d2c
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-26 13:39:18 -0500

    Refactor controller and move all logic into poro

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -5,6 +5,8 @@ class Api::V1::RoadTripController < ApplicationController
     # Returns error message if data was passed through URL
     if params[:road_trip].blank?
       json_response({ "error": {"message" => 'missing request body'} }, :bad_request)
+    # Ensure all params are present in request body
+    # Return error if any params are empty string or missing
     elsif (params[:road_trip][:origin].blank?) || (params[:road_trip][:destination].blank?) || (params[:road_trip][:api_key].blank?)
       json_response({ "error": {"message" => 'required parameters are missing or empty'} }, :bad_request)
     else 
@@ -12,11 +14,7 @@ class Api::V1::RoadTripController < ApplicationController
       # Checking API key against User database
       user = User.find_by(api_key: params[:road_trip][:api_key])
       if user
-        get_destination_coordinates(params[:road_trip][:destination])
-        get_route(params[:road_trip][:origin], params[:road_trip][:destination])
-        get_condition_at_eta(@destination_coordinates[:lat], @destination_coordinates[:lng], @travel_time)
-
-        roadtrip = Roadtrip.new(params[:road_trip][:origin], params[:road_trip][:destination], @travel_time, @condition_at_eta)
+        roadtrip = Roadtrip.new(params[:road_trip][:origin], params[:road_trip][:destination])
         
         # Serialized JSON response if all authentication checks pass
         json_response(RoadtripSerializer.new(roadtrip))
@@ -26,32 +24,4 @@ class Api::V1::RoadTripController < ApplicationController
       end
     end 
   end
-
-  private 
-
-    def get_route(start, destination)
-      directions = MapquestService.directions(start, destination)
-      if directions[:info][:statuscode] == 402
-        @travel_time = 'impossible route'
-      else 
-        @travel_time = directions[:route][:formattedTime]
-      end
-    end
-
-    def get_destination_coordinates(location)
-      @destination_coordinates = MapquestService.get_city_coordinates(location)
-    end
-    
-    def get_condition_at_eta(latitude, longitude, travel_time)
-      if travel_time != 'impossible route'
-        hours_to_eta = travel_time.split(":")[0].to_i
-        if hours_to_eta <= 49
-          @condition_at_eta = OpenweatherFacade.hourly_weather(latitude, longitude)[hours_to_eta - 1]
-        else
-          @condition_at_eta = OpenweatherFacade.hourly_weather(latitude, longitude)[-1]
-        end
-      else 
-        'nil'
-      end
-    end
 end
