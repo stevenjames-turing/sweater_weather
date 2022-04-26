f98feb91f759ecf31b9a41982085bf58a55cdc0b
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-26 12:10:29 -0500

    Refactor: update facade to use case statement. combine 3 methods into 1

--- app/facades/openweather_facade.rb
+++ app/facades/openweather_facade.rb
@@ -1,26 +1,20 @@
 class OpenweatherFacade
 
-  def self.daily_weather(latitude, longitude)
-    json = OpenweatherService.all_weather(latitude, longitude)
+  def self.weather_forecast(location, forecast_type)
+    coordinates = MapquestService.get_city_coordinates(location)
+    json = OpenweatherService.all_weather(coordinates[:lat], coordinates[:lng])
 
-    # Creates objects for only the next 5 days
-    daily_weather = json[:daily][0..4].map do |day_data|
-      DailyWeather.new(day_data)
+    case forecast_type
+      when "current"
+        CurrentWeather.new(json[:current])
+      when "hourly"
+        hourly_weather = json[:hourly].map do |hour_data|
+          HourlyWeather.new(hour_data)
+        end
+      when "daily"
+        daily_weather = json[:daily][0..4].map do |day_data|
+          DailyWeather.new(day_data)
+        end
     end
-  end
-
-  def self.hourly_weather(latitude, longitude)
-    json = OpenweatherService.all_weather(latitude, longitude)
-
-    # Creates objects for only the next 8 hours
-    hourly_weather = json[:hourly].map do |hour_data|
-      HourlyWeather.new(hour_data)
-    end
-  end
-
-  def self.current_weather(latitude, longitude)
-    json = OpenweatherService.all_weather(latitude, longitude)
-
-    CurrentWeather.new(json[:current])
   end
 end
