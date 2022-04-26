7bb8c9dfded0ad7c3538cbc0929a1758564e751f
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-26 13:39:34 -0500

    Refactor to include all logic and remove from controller

--- app/poros/roadtrip.rb
+++ app/poros/roadtrip.rb
@@ -1,24 +1,39 @@
 class Roadtrip 
   include DateTimeFormatter
 
-    attr_reader :id, :start_city, :end_city, :travel_time, :weather_at_eta
+  attr_reader :id, :start_city, :end_city, :travel_time, :weather_at_eta
 
-    def initialize(start_city, destination, travel_time, weather_data) 
-        @id = 'null'
-        @start_city = start_city
-        @end_city = destination
-        @travel_time = travel_time != 'impossible route' ? time_formatter(travel_time) : travel_time
-        @weather_at_eta = format_weather(weather_data)
-    end
+  def initialize(start_location, destination) 
+      @id = 'null'
+      @start_city = start_location
+      @end_city = destination
+      @travel_time = calculate_travel_time(start_location, destination)
+      @weather_at_eta = destination_weather_at_eta(destination)
+  end
 
-    def format_weather(weather_data)
-      if @travel_time == 'impossible route'
-        forecast = {}
-      else 
-        forecast = {
-                      "temperature": weather_data.temperature,
-                      "conditions": weather_data.conditions
-                    }
+  def calculate_travel_time(start_location, destination)
+    directions = MapquestService.directions(start_city, destination)
+
+    directions[:info][:statuscode] != 402 ? directions[:route][:formattedTime] : 'impossible route'
+  end
+
+  def destination_weather_at_eta(destination)
+    if @travel_time != 'impossible route'
+      hours_to_eta = @travel_time.split(":")[0].to_i
+
+      if hours_to_eta <= 49
+        weather_at_eta = OpenweatherFacade.weather_forecast(destination, 'hourly')[hours_to_eta - 1]
+      else
+        weather_at_eta = OpenweatherFacade.weather_forecast(destination, 'hourly')[-1]
       end
+
+      forecast = {
+                    "temperature": weather_at_eta.temperature,
+                    "conditions": weather_at_eta.conditions
+                  }
+    else 
+      forecast = {}
     end
+    return forecast
+  end
 end
