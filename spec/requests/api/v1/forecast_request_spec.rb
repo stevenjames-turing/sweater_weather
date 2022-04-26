56005373cd6dbd6109688efd4ef9465410e7cd0c
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-26 12:19:15 -0500

    Update tests for controller refactors

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -35,8 +35,8 @@ describe 'Forecast API', :vcr do
         expect(attributes[:current_weather][:datetime]).to be_a String
         expect(attributes[:current_weather][:sunrise]).to be_a String
         expect(attributes[:current_weather][:sunset]).to be_a String
-        expect(attributes[:current_weather][:temperature]).to be_a Float
-        expect(attributes[:current_weather][:feels_like]).to be_a Float
+        expect(attributes[:current_weather][:temperature]).to be_a(Float) | be_an(Integer)
+        expect(attributes[:current_weather][:feels_like]).to be_a(Float) | be_an(Integer)
         expect(attributes[:current_weather][:humidity]).to be_a(Float) | be_an(Integer)
         expect(attributes[:current_weather][:uvi]).to be_a(Float) | be_an(Integer)
         expect(attributes[:current_weather][:visibility]).to be_a(Float) | be_an(Integer)
