48ac16fb2f16d025269f5b2f57d1e781cd0ea1c3
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-26 14:07:12 -0500

    Test: roadtrip returns weather even if eta is longer than 48 hours

--- spec/poros/roadtrip_spec.rb
+++ spec/poros/roadtrip_spec.rb
@@ -23,14 +23,25 @@ RSpec.describe Roadtrip, :vcr do
     expect(roadtrip.travel_time).to_not eq('impossible route')
     expect(roadtrip.weather_at_eta.keys).to eq([:temperature, :conditions])
   end
-                                
+  
   it 'returns an empty hash of weather data if route is not possible' do 
     roadtrip = Roadtrip.new(@start_location, @impossible_route)
-
+    
     expect(roadtrip.id).to eq('null')
     expect(roadtrip.start_city).to eq('st. louis, mo')
     expect(roadtrip.end_city).to eq('london, UK')
     expect(roadtrip.travel_time).to eq("impossible route")
     expect(roadtrip.weather_at_eta).to eq({})
   end
+  
+  it 'returns weather at the 48th hour if travel time is longer than 48 hours' do 
+    roadtrip = Roadtrip.new('anchorage, ak', 'miami, fl')
+    
+    expect(roadtrip.id).to eq('null')
+    expect(roadtrip.start_city).to eq('anchorage, ak')
+    expect(roadtrip.end_city).to eq('miami, fl')
+    expect(roadtrip.travel_time).to_not eq("impossible route")
+    expect(roadtrip.weather_at_eta.keys).to eq([:temperature, :conditions])
+  end
+
 end
