b6baef854c92e84864297072ecd905237c4d7864
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-26 12:20:13 -0500

    Refactor controller and add code comments

--- app/facades/openweather_facade.rb
+++ app/facades/openweather_facade.rb
@@ -5,16 +5,16 @@ class OpenweatherFacade
     json = OpenweatherService.all_weather(coordinates[:lat], coordinates[:lng])
 
     case forecast_type
-      when "current"
-        CurrentWeather.new(json[:current])
-      when "hourly"
-        hourly_weather = json[:hourly].map do |hour_data|
-          HourlyWeather.new(hour_data)
-        end
-      when "daily"
-        daily_weather = json[:daily][0..4].map do |day_data|
-          DailyWeather.new(day_data)
-        end
+    when "current"
+      CurrentWeather.new(json[:current])
+    when "hourly"
+      hourly_weather = json[:hourly].map do |hour_data|
+        HourlyWeather.new(hour_data)
+      end
+    when "daily"
+      daily_weather = json[:daily][0..4].map do |day_data|
+        DailyWeather.new(day_data)
+      end
     end
   end
 end
